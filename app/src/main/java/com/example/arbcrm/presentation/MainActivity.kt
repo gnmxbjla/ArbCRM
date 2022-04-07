6d68eae12c47794d65e703b4e0c0d130f4dbfc68
filipkof <71509204+svegoe@users.noreply.github.com>
2022-04-08 02:12:54 +0300

    Нихуя не работают расчеты, нужно починить

--- app/src/main/java/com/example/arbcrm/presentation/MainActivity.kt
+++ app/src/main/java/com/example/arbcrm/presentation/MainActivity.kt
@@ -3,10 +3,25 @@ package com.example.arbcrm.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.arbcrm.R
+import com.example.arbcrm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate (savedInstanceState: Bundle?) {
+        super. onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonResult.setOnClickListener {
+            val a = binding.StavkaEditTextNumber.toString().toInt()
+            val b = binding.LeadEditTextNumber.toString().toInt()
+            val c = binding.ApruvEditTextNumber.toString().toInt()
+            val d = binding.TrashEditTextNumber.toString().toInt()
+
+            val sum = a+b+c+d
+
+            binding.RoiResulttextView.text = sum.toString()
+
+        }
     }
 }
