c7217048623faa58e3568d374abfe2feaafd4379
filipkof <71509204+svegoe@users.noreply.github.com>
2022-04-11 15:26:42 +0300

    Оно считает, нужно переделать на подсчет рои и цены лида в 0

--- app/src/main/java/com/example/arbcrm/presentation/MainActivity.kt
+++ app/src/main/java/com/example/arbcrm/presentation/MainActivity.kt
@@ -2,26 +2,26 @@ package com.example.arbcrm.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.example.arbcrm.R
-import com.example.arbcrm.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    override fun onCreate (savedInstanceState: Bundle?) {
-        super. onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.buttonResult.setOnClickListener {
-            val a = binding.StavkaEditTextNumber.toString().toInt()
-            val b = binding.LeadEditTextNumber.toString().toInt()
-            val c = binding.ApruvEditTextNumber.toString().toInt()
-            val d = binding.TrashEditTextNumber.toString().toInt()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_result.setOnClickListener {
+            val a = StavkaEditTextNumber.text.toString().toInt()
+            val b = LeadEditTextNumber.text.toString().toInt()
+            val c = ApruvEditTextNumber.text.toString().toInt()
+            val d = TrashEditTextNumber.text.toString().toInt()
 
             val sum = a+b+c+d
 
-            binding.RoiResulttextView.text = sum.toString()
-
+            RoiResulttextView.text = sum.toString()
         }
     }
+
 }
